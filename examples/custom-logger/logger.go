051a2ac777aada00ca91c5fa534d719133c48be4
小草 <2802083538@qq.com>
2022-01-21 11:45:08 +0800

    Update logger.go

--- examples/custom-logger/logger.go
+++ examples/custom-logger/logger.go
@@ -81,7 +81,7 @@ func output(v ...interface{}) string {
 	file = filepath.Base(file)
 	funcName := strings.TrimPrefix(filepath.Ext(runtime.FuncForPC(pc).Name()), ".")
 
-	logFormat := "%s %s:%d:%s" + fmt.Sprint(v...) + "\n"
+	logFormat := "%s %s:%d:%s " + fmt.Sprint(v...) + "\n"
 	date := time.Now().Format("2006-01-02 15:04:05")
 	return fmt.Sprintf(logFormat, date, file, line, funcName)
 }
