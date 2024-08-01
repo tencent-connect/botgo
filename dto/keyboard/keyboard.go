0314b91ef7843107e61e21bb59e027134452a57e
shannonchen <shannonchen@tencent.com>
2024-08-01 11:45:18 +0800

    feat(botgo): 修改内联键盘SubscribeData tag定义 (merge request !95)
    Squash merge branch 'feat_mod_action_shannonchen' into 'master'
    feat(botgo): 修改内联键盘SubscribeData tag定义

--- dto/keyboard/keyboard.go
+++ dto/keyboard/keyboard.go
@@ -67,7 +67,7 @@ type Action struct {
 	ClickLimit           uint32        `json:"click_limit,omitempty"`              // 可点击的次数, 默认不限
 	Data                 string        `json:"data,omitempty"`                     // 操作相关数据
 	AtBotShowChannelList bool          `json:"at_bot_show_channel_list,omitempty"` // false:当前 true:弹出展示子频道选择器
-	SubscribeData        SubscribeData `json:"subscribe_data"`                     // 订阅按钮数据，type=ActionTypeSubscribe时使用
+	SubscribeData        SubscribeData `json:"subscribe_data,omitempty"`           // 订阅按钮数据，type=ActionTypeSubscribe时使用
 	Modal                *Modal        `json:"modal,omitempty"`                    // 用户点击二次确认操作
 }
 
